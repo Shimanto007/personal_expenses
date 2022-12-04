02f79b37e8b481c9bb3032d3a90d73c3ad318edf
atreeboot <shimantoray007@gmail.com>
2022-12-04 15:57:40 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import './widgets/user_transactions.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal Expenses App'),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.purple,
+              child: Text(
+                'Chart!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              elevation: 5,
+            ),
+          ),
+          UserTransactions(),
+        ],
+      ),
+    );
+  }
+}
